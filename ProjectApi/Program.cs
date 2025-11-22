efd2fbe821c7c631a547f6b946763c3b13b5a783
aptikum <vc615774@gmail.com>
2025-11-22 23:27:44 +0500

    mvp

--- ProjectApi/Program.cs
+++ ProjectApi/Program.cs
@@ -30,12 +30,15 @@ builder.Services.AddScoped<IKidTaskService, KidTaskService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-    
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
